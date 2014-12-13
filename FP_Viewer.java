e80cc5df143b87c584ca7f7f83478256e1f48f1b
Mitchell Mohorovich <mitchell.mohorovich@gmail.com>
2014-12-13 15:50:40 -0500

    Made window not resizeable.

--- FP_Viewer.java
+++ FP_Viewer.java
@@ -10,6 +10,7 @@ public class FP_Viewer
       frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
       frame.setSize(WIDTH, HEIGHT);
       frame.setTitle("Flappy Panar");
+      frame.setResizable(false);
       frame.setVisible(true);
    }
 }
