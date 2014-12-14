59408ffe4b0b55e7148bd4cba340c8b69d3d1466
Mitchell Mohorovich <mitchell.mohorovich@gmail.com>
2014-12-14 16:27:42 -0500

    Gradient goes through phases of horrid colours now

--- FP_Start_Layer_UI.java
+++ FP_Start_Layer_UI.java
@@ -13,39 +13,78 @@ import java.awt.image.BufferedImage;
 import java.awt.image.BufferedImageOp;
 import java.awt.image.ConvolveOp;
 import java.awt.image.Kernel;
+import java.awt.event.MouseEvent;
+import javax.swing.JLayer;
+
+/**
+ * Divided into 5 phases of colour changes, this layer draws horrible hideous and eye bunring colours on the game to make it horrible to look at.
+ */
 
 public class FP_Start_Layer_UI extends LayerUI<JComponent>
 {
-   public static final String start_string = "Click to start the rocket science!";
-   private BufferedImage mOffscreenImage;
-   private BufferedImageOp mOperation;
-
+   int phase;
+   int phase_counter;
+   private Color top;
+   private Color bottom;
    public FP_Start_Layer_UI()
    {
-      float ninth = 1.0f / 9.0f;
-      float[] blurKernel = {
-         ninth, ninth, ninth,
-         ninth, ninth, ninth,
-         ninth, ninth, ninth
-      };
-      mOperation = new ConvolveOp(
-            new Kernel(3, 3, blurKernel),
-            ConvolveOp.EDGE_NO_OP, null);
+      phase = 1;
+      phase_counter = 0;
+
+      top = Color.green;
+      bottom = Color.yellow;
+      System.out.println(top+" "+bottom);
+   }
+
+   protected void processMouseEvent(MouseEvent e, JLayer<? extends JComponent> l)
+   {
+      System.out.println(e);
    }
 
    public void paint(Graphics g, JComponent c)
    {
       super.paint(g, c);
 
-      Graphics2D g2 = (Graphics2D) g.create();
+      if(phase_counter == 255){
+         phase_counter = 0;
+         if(phase == 4) 
+            phase = 1;
+         else 
+            phase++;
+      }
 
-      g2.drawString(start_string, 100, 100);
+      switch(phase) {
+         case 1: 
+            top = new Color(top.getRed()+1, top.getGreen(), top.getBlue());
+            bottom = new Color(bottom.getRed()-1, bottom.getGreen()-1, bottom.getBlue()+1);
+            phase_counter++;
+            break; 
+         case 2:
+            top = new Color(top.getRed()-1, top.getGreen()-1, top.getBlue()+1);
+            bottom = new Color(bottom.getRed()+1, bottom.getGreen(), bottom.getBlue()-1);
+            phase_counter++;
+            break;
+         case 3:
+            top = new Color(top.getRed()+1, top.getGreen(), top.getBlue()-1);
+            bottom = new Color(bottom.getRed()-1, bottom.getGreen()+1, bottom.getBlue());
+            phase_counter++;
+            break;
+         case 4:
+            top = new Color(top.getRed()-1, top.getGreen()+1, top.getBlue());
+            bottom = new Color(bottom.getRed()+1, bottom.getGreen(), bottom.getBlue());
+            phase_counter++;
+            break;
+      }
+
+
+
+      Graphics2D g2 = (Graphics2D) g.create();
 
       int w = c.getWidth();
       int h = c.getHeight();
       g2.setComposite(AlphaComposite.getInstance(
                AlphaComposite.SRC_OVER, .5f));
-      g2.setPaint(new GradientPaint(0, 0, Color.green, 0, h, Color.yellow));
+      g2.setPaint(new GradientPaint(0, 0, top, 0, h, bottom));
       g2.fillRect(0, 0, w, h);
 
       g2.dispose();
